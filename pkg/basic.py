940864f2e3e111eff3ba159b651f01b31163663d
chris <chiachun0920@gmail.com>
2020-05-16 10:19:23 +0000

    Add: unordered list implementation - 1

--- pkg/basic.py
+++ pkg/basic.py
@@ -2,3 +2,16 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def add(self, item):
+        node = Node(item)
+        node.next = self.head
+        self.head = node
