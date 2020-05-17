bcc78509835d59d584824677a52b7a9366d5d0b0
chris <chiachun0920@gmail.com>
2020-05-17 08:46:13 +0000

    Add: unordered list implementation - 2

--- pkg/basic.py
+++ pkg/basic.py
@@ -15,3 +15,32 @@ class UnorderedList:
         node = Node(item)
         node.next = self.head
         self.head = node
+
+    def size(self):
+        count = 0
+        current = self.head
+        while current != None:
+            count = count + 1
+            current = current.next
+        return count
+
+    def search(self, value):
+        current = self.head
+        while current != None:
+            if current.data == value:
+                return True
+            current = current.next
+        return False
+
+    def remove(self, value):
+        previous = None
+        current = self.head
+        while current != None:
+            if current.data == value:
+                if previous is None:
+                    self.head = current.next
+                else:
+                    previous.next = current.next
+                return
+            previous = current
+            current = current.next
--- tests/test_unordered_list.py
+++ tests/test_unordered_list.py
@@ -7,10 +7,43 @@ def unordered_list():
     return UnorderedList()
 
 
-def test_unordered_list(unordered_list):
+def test_unordered_list_adding_item(unordered_list):
     assert unordered_list.head is None
     assert unordered_list.is_empty() is True
+    assert unordered_list.size() is 0
 
     unordered_list.add(7)
 
     assert unordered_list.head is not None
+    assert unordered_list.head.data is 7
+    assert unordered_list.head.next is None
+    assert unordered_list.size() is 1
+    assert unordered_list.search(7) is True
+    assert unordered_list.search(8) is False
+
+    unordered_list.add('A')
+
+    assert unordered_list.head.data is 'A'
+    assert unordered_list.head.next.data is 7
+    assert unordered_list.head.next.next is None
+    assert unordered_list.size() is 2
+    assert unordered_list.search('A') is True
+    assert unordered_list.search('B') is False
+
+
+def test_unordered_list_removing_item(unordered_list):
+    unordered_list.add('foo')
+    unordered_list.add('bar')
+    unordered_list.add('goo')
+    unordered_list.add('gle')
+
+    assert unordered_list.size() == 4
+
+    unordered_list.remove('gle')
+
+    assert unordered_list.size() == 3
+    assert unordered_list.search('gle') == False
+
+    unordered_list.remove('foo')
+    assert unordered_list.size() == 2
+    assert unordered_list.search('foo') == False
